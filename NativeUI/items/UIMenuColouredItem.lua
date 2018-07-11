fca7584aba7cf534cc0d450e40a948d796b74259
Frazzle <28698343+FrazzIe@users.noreply.github.com>
2018-07-11 18:57:47 +0100

    Fix for issue #7

--- NativeUI/items/UIMenuColouredItem.lua
+++ NativeUI/items/UIMenuColouredItem.lua
@@ -111,6 +111,7 @@ function UIMenuColouredItem:SetRightBadge(Badge)
 end
 
 function UIMenuColouredItem:Draw()
+	self.Rectangle:Size(431 + self.ParentMenu.WidthOffset, self.Rectangle.Height)
 	self.Rectangle:Draw()
 	self.Base:Draw()
 end
